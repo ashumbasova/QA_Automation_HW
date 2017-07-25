06ba449d9f8279898c44778abfff390f597b5f7a
Anna Shumbasova <ashumbasova@gmail.com>
2017-07-25 20:37:06 +0300

    Draft of HW5 @elpiankova your comments are highly appreciated

--- HW5/rest_api.py
+++ HW5/rest_api.py
@@ -4,18 +4,18 @@ class ApiTest:
     def __init__(self, url, newRole):
         self.url = url
         self.newRole = newRole
-        self.roleID = 0
+        self.roleID = None
 
     def post(self):
         res = requests.post(self.url, data=self.newRole)
         self.roleID = res.json()['id']
         return res
 
-    def get(self, bookUrl):
-        return requests.get(bookUrl)
+    def get(self, roleUrl):
+        return requests.get(roleUrl)
 
-    def put(self, bookUrl, data):
-        return requests.put(bookUrl, data=data)
+    def put(self, roleUrl, data):
+        return requests.put(roleUrl, data=data)
 
-    def delete(self, bookUrl):
-        return requests.delete(bookUrl)
+    def delete(self, roleUrl):
+        return requests.delete(roleUrl)
--- HW5/test_rest_api.py
+++ HW5/test_rest_api.py
@@ -16,10 +16,30 @@ class Tests(unittest.TestCase):
         self.role.post()
         self.roleUrl = self.role.url + str(self.role.roleID) + '/'
 
-    def test1(self):
-        self.assertEqual(self.role.get(self.roleUrl).json()["name"], 'Sir Magician')
-        self.assertEqual(self.role.get(self.roleUrl).json()["level"], 500)
-        self.assertEqual(self.role.put(self.roleUrl, {'name': 'Anna S'}).json()['name'], 'Anna S')
+        # def test_post(self):
+        #     self.assertEqual(self.role.post().status_code, 201)
+        #     self.role.delete(self.roleUrl)
+
+    def test_get(self):
+        res = self.role.get(self.roleUrl)
+        self.assertEqual(res.json()["name"], 'Sir Magician')
+        self.assertEqual(res.json()["type"], 'Senior Wizard')
+        self.assertEqual(res.json()["level"], 500)
+        self.assertEqual(res.json()['book'], 1)
+        self.assertEqual(res.status_code, 200)
+
+    def test_put(self):
+        res = self.role.put(self.roleUrl, {'name': 'Anna S', 'type': 'QA'})
+        self.assertEqual(res.json()['name'], 'Anna S')
+        self.assertEqual(res.json()['type'], 'QA')
+        self.assertEqual(res.status_code, 200)
+
+    def test_del(self):
+        self.assertEqual(self.role.delete(self.roleUrl).status_code, 204)
+
+        # def test_get_neg(self):
+        #     self.assertEqual(self.role.get(self.roleUrl), 400)
+
 
     def tearDown(self):
         self.role.delete(self.roleUrl)
